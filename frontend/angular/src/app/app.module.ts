53f6c63a05c5e1b55f8c296c56d8ea55b3e3898a
Arthur <arthur.zhukovski@gmail.com>
2017-04-14 17:44:19 +0300

    Implement basic authorization/login functionality

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -4,16 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule} from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
+// used to create fake backend
+import { fakeBackendProvider } from './helpers/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { UserTableComponent } from './components/user-table/user-table.component';
 import { UserDataItemComponent } from './components/user-data-item/user-data-item.component';
+import { AlertComponent } from './components/alert/index';
+import { AuthGuard } from './services/index';
+import { AlertService, AuthenticationService, UserService } from './services/index';
+import { HomeComponent } from './components/home/index';
+import { LoginComponent } from './components/login/index';
+import { RegisterComponent } from './components/register/index';
 
 const appRoutes: Routes = [
-  { path: 'user-table', component: UserTableComponent
-  },
-  {
-    path: '123', redirectTo: ''
-  }
+  { path: 'user-table', component: UserTableComponent, canActivate: [AuthGuard]},
+  { path: 'profile', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '*', redirectTo: '' }
 
 ];
 
@@ -21,7 +34,11 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     UserTableComponent,
-    UserDataItemComponent
+    UserDataItemComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +47,17 @@ const appRoutes: Routes = [
     JsonpModule,
     RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/angular/src/app/services/index.ts
@@ -0,0 +1,4 @@
+export * from './alert/alert.service';
+export * from './authentication/authentication.service';
+export * from './user/user.service';
+export * from './auth-guard/auth.guard';
